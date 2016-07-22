e91b83e3d14e0f5bec51dbede6d9b4eb5d227452
priyavpd <vpriyacse2@gmail.com>
2016-07-22 13:58:12 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,15 @@
+public class OddEven
+{
+public static void main(String[] args)
+{
+int a=10;
+if(a%2==0)
+{
+System.out.println("even number");
+}
+else
+{
+System.out.println("odd number");
+}
+}
+}
