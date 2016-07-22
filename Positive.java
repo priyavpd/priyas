079baf3594698395b6b4ad945f1ff84d98631c7e
priyavpd <vpriyacse2@gmail.com>
2016-07-22 13:52:18 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,16 @@
+# priyas
+public class Positive
+{
+public static void main(String[] args)
+{
+int a=10;
+if(a>0)
+{
+System.out.println("postive number");
+}
+else
+{
+System.out.println("negative number");
+}
+}
+}
